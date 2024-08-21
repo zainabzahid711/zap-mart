2a5d3c92a5d8f3bcb4eb99051657254b805cf84b
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2024-08-21 15:44:38 +0500

    build the basicStructure of UI

---
+++ app/components/nav/navBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link"
+import Container from "../container"
+import { Redressed } from "next/font/google"
+
+const redressed = Redressed ({subsets: ['latin'], weight: ['400']})
+
+const NavBar = () =>{
+    return(
+        <>
+        <div className="sticky top-0 w-full bg-navblue z-30 shadow-sm">
+           <div className="py-5 border-b-[1px]">
+            <Container>
+               <div className="flex items-center justify-between gap-12 md:gap-0">
+                    <Link href="/" className={`${redressed.className} text-center font-bold text-4xl `}>zapmart</Link>
+                    <div className="hidden md:block">Search</div>
+                <div className="flex items-center gap-8 md:gap-12 ">
+                   <div>CartMenu</div>
+                   <div>Usermenu</div>
+                </div>
+               </div>
+            </Container>
+           </div>
+        </div>
+        </>
+    )
+}
+
+export default NavBar
