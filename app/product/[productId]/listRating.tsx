b607a72e164844e971e5fef3d8672ca1dad117bb
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2024-08-29 00:58:15 +0500

    set the reviewImages

--- app/product/[productId]/listRating.tsx
+++ app/product/[productId]/listRating.tsx
@@ -4,6 +4,7 @@ import Heading from "@/app/components/heading";
 import React from "react";
 import moment from "moment";
 import { Rating } from "@mui/material";
+import Avatar from "@/app/components/avatar";
 
 interface ListRatingProps {
   product: any;
@@ -18,16 +19,16 @@ const ListRating: React.FC<ListRatingProps> = ({ product }) => {
           {product.reviews &&
             product.reviews.map((review: any) => {
               return (
-                <div key={review.id} className="max-w-300px">
+                <div key={review.id} className="max-w-[300px]">
                   <div className="gap-2 flex items-center">
-                    <div>avatar</div>
+                    <Avatar src={review?.user.image} />
                     <div className="font-semibold">{review?.user.name} </div>
                     <div className="font-light">
                       {moment(review.createDate).fromNow()}{" "}
                     </div>
                   </div>
                   <div className="mt-2">
-                    <Rating value={review.rating} readOnly />{" "}
+                    <Rating value={review.rating} readOnly />
                     <div className="mt-2">{review.comment} </div>
                     <hr className="mt-4 mb-4" />
                   </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com", "m.media-amazon.com"],
+    domains: [
+      "firebasestorage.googleapis.com",
+      "m.media-amazon.com",
+      "lh3.googleusercontent.com",
+    ],
   },
 };
 
